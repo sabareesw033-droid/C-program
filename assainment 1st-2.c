d9482cab0d169ed4bfebe507bc933caa5fe1c366
sabareesw033-droid <sabareesw033@gmail.com>
2026-05-19 18:45:42 +0530

    Add files via upload

---
+++ assainment 1st-2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	printf("i am sabareeswaran\v");
+	printf("i am currently pursuing at sscet\v");
+    printf("i am innocent\v");
+    printf("i am from biomedical engineering\v");
+    printf("my native place is kallakurichi\v");
+    printf("my father name is veerapathiran\v");
+    printf("my mother name is rajakumari\v");
+    printf("i am always alone\v");
+    printf("i am always using mobile daily\v");
+    printf("i am sleep 8 hours daily\v");
+    printf("i am good listning\v");
+    printf("i am currently learning c programming language\v");
+    printf("i am before coming from home\v");
+    printf("i am happy to be part of inspire'26 program\v");
+    printf("i am attending inspire'26 program\v");
+    printf("i am present in the class daily\v");
+    printf("i am puntualy attending correct time\v");
+    printf("i have little knowledge about commmunication\v");
+    printf("i am stayed at hostel\v");
+    printf("i currently 10 days to learn c programming language\v");
+    printf("i am enjoyes my school days\v");
+    printf("i am learning little bit about politics\v");
+    printf("i am voting in 2026 election\v");
+    printf("i am put my vote first time 2026\v");
+    printf("i am good at mathematics\v");
+    printf("i want to become a biomedical engineer\v");
+}
