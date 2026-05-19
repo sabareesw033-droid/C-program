61a7cdda36c0075038853dc0c40308b66da9ba7f
sabareesw033-droid <sabareesw033@gmail.com>
2026-05-19 18:45:12 +0530

    Add files via upload

---
+++ assaigment 2nd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main() {
+    //basic data types = char,int,float,double
+int a=10;
+int b=8;
+int c=6;
+int age=a*a;
+char grade= 'l';
+float marks=(a*a*a);
+double values=((a*a)-(b+c))/1.2;
+printf("age=%d\n",age);
+printf("grade=%c\n",grade);
+printf("marks=%f\n",marks);
+printf("values=%1f\n",values);
+}
