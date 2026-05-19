6e151c2da05a2dac2cfd800876de04c927972d33
sabareesw033-droid <sabareesw033@gmail.com>
2026-05-19 18:47:11 +0530

    Add files via upload

---
+++ assaignment 1st-5 nn.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	printf("i am sabareeswaran\n\n");
+	printf("i am currently pursuing at sscet\n\n");
+    printf("i am innocent\n\n");
+    printf("i am from biomedical engineering\n\n");
+    printf("my native place is kallakurichi\n\n");
+    printf("my father name is veerapathiran\n\n");
+    printf("my mother name is rajakumari\n\n");
+    printf("i am always alone\n\n");
+    printf("i am always using mobile daily\n\n");
+    printf("i am sleep 8 hours daily\n\n");
+    printf("i am good listning\n\n");
+    printf("i am currently learning c programming language \n\n");
+    printf("i am before coming from home\n\n");
+    printf("i am happy to be part of inspire'26 program\n\n");
+    printf("i am attending inspire'26 program\n\n");
+    printf("i am present in the class daily\n\n");
+    printf("i am puntualy attending correct time\n\n");
+    printf("i have little knowledge about commmunication\n\n");
+    printf("i am stayed at hostel\n\n");
+    printf("i currently 10 days to learn c programming language\n\n");
+    printf("i am enjoyes my school days\n\n");
+    printf("i am learning little bit about politics\n\n");
+    printf("i am voting in 2026 election\n\n");
+    printf("i am put my vote first time 2026\n\n");
+    printf("i am good at mathematics\n\n");
+    printf("i want to become a biomedical engineer\n\n");
+}
