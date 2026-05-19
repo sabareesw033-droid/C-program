44860b5d0f8a500f5ba28ff33c7f8026e747745e
sabareesw033-droid <sabareesw033@gmail.com>
2026-05-19 18:48:22 +0530

    Add files via upload

---
+++ assaignment 4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+char main() {
+char age=1+11+7;
+char subjects=111-9;
+char semaster=3+5;
+char years=2+2;
+char reading=2*90;
+char writing=100-90;
+char mobile=24/7;
+char hour=2*12;
+char students=100/8;
+char working=3-1;
+char holidays=1605-1399;
+char staff=4*6;
+char eating=2*4;
+char fuel=9*6;
+char milk=17/17;
+char ticket=5000/50;
+printf("age=%d\n",age);
+printf("subjects=%d\n",subjects);
+printf("semaster=%d\n",semaster);
+printf("years=%d\n",years);
+printf("reading=%d\n",reading);
+printf("writing=%d\n",writing);
+printf("mobile=%d\n",mobile);
+printf("hour=%d\n",hour);
+printf("students=%d\n",students);
+printf("working=%d\n",working);
+printf("holidays=%d\n",holidays);
+printf("staff=%d\n",staff);
+printf("eating=%d\n",eating);
+printf("fuel=%d\n",fuel);
+printf("milk=%d\n",milk);
+printf("ticket=%d\n",ticket);
+}
