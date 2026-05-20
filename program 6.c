05d99c9559d486491838b7bbe31b30021118943d
sabareesw033-droid <sabareesw033@gmail.com>
2026-05-20 13:45:42 +0530

    Add files via upload

---
+++ program 6.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+ {
+    
+    int A;
+    char B;
+    float C;
+    long D;
+    double E;
+    unsigned int F;
+
+    printf("the size of integer =%d bytes\n\n",sizeof(A));
+    printf(" the size of character is %d bytes\n\n",sizeof(B));
+    printf("the size of float is %d bytes\n\n",sizeof(C));
+    printf("the size of long is %d bytes\n\n",sizeof(D));
+    printf("the size of double  %d bytes\n\n",sizeof(E));
+    printf("the size of unsigned is %d bytes\n\n",sizeof(F));
+}
