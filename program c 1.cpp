d2fbb00805b377f3d614f208706651d3299572a3
sabareesw033-droid <sabareesw033@gmail.com>
2026-05-20 13:43:21 +0530

    Add files via upload

---
+++ program c 1.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+/*hello how are you 
+this is at sscet on 2026
+for students */
+
+int A;
+int B,C;
+int D=10;
+int E,F=18,G;
+int H=12,I=20,J=40;
+/* values assigned is called initializing of that variables */
+
+}
