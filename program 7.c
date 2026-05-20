c0a42899f78a2eb937cb35de6323e8c7c82fd791
sabareesw033-droid <sabareesw033@gmail.com>
2026-05-20 13:46:04 +0530

    Add files via upload

---
+++ program 7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    printf("INTEGER IS = %d BYTES \n\n",sizeof(int));
+    printf("CHARACTER IS =%d BYTES \n\n ",sizeof(char));
+    printf("DOUBLE IS =%d BYTES \n\n",sizeof(double));
+    printf("FLOAT IS =%d BYTES \n\n ",sizeof(float));
+    printf("LONG IS = %d BYTES \n\n",sizeof(long));
+    printf("SIGNED IS =%d BYTES \n\n",sizeof(signed));
+    printf("UNSIGNED IS =%d BYTES \n\n",sizeof(unsigned));
+    printf("LONG DOUBLE IS =%d BYTES \n\n ",sizeof(long double));
+}
