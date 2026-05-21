432bd15282759eca2f1b4dd88ba1ff8ca7d1d56b
sabareesw033-droid <sabareesw033@gmail.com>
2026-05-21 21:08:42 +0530

    Add files via upload

---
+++ 7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a=10;
+    printf("the value of now is %d :",a);
+
+    /* the icrement operator method 1*/
+
+    a++;
+
+     printf("the value of after a increment is %d :",a);
+}
