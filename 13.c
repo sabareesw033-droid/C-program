c8f7d664e43b06ebc0add02029d22d334f0c839f
sabareesw033-droid <sabareesw033@gmail.com>
2026-05-21 21:10:52 +0530

    Add files via upload

---
+++ 13.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int a;
+    
+    printf("enter your marks ");
+    scanf("%d",&a);
+
+    if(a>50)
+    printf("pass");
+
+    else
+    printf("fail");
+    return 0;
+}
