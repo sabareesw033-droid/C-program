a39c4eb7d04d70a88e1e8a5e9e69e330fc2603e8
sabareesw033-droid <sabareesw033@gmail.com>
2026-05-21 21:35:42 +0530

    Add files via upload

---
+++ prog0 3.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+	int A,B,Answer;
+	
+	printf("ENTER THE VALUE FOR A :");
+	
+	scanf("%d",&A);
+	
+	printf("- - - - - - - - - - - -\n");
+	
+	Answer=A+B;
+	
+	printf("Hello ,my friend you enter %d :\n",A);
+	
+	printf("ENTER THE VALUE FOR B:");
+	
+	scanf("%d",&B);
+	
+	printf("- - - - - - - - - - - -\n");
+	
+	printf("dai you enter %d :\n",B);
+	
+	
+	
+	printf("rendu no ad panna %d varum\n",Answer);
+	
+	 Answer = A-B;
+	
+	printf("rendu no minus panna %d varum\n",Answer);
+	
+		Answer = A*B;
+	
+	printf("rendu no multiply panna %d varum\n",Answer);
+	
+		Answer = A/B;
+	
+	printf("rendu no divide panna %d varum\n",Answer);
+}
