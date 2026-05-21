f1e0d2d951216a5ae78f2e1d73016f27bdc3605e
sabareesw033-droid <sabareesw033@gmail.com>
2026-05-21 21:14:11 +0530

    Add files via upload

---
+++ 17.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int A=0;
+
+    
+     sscet: while(A<20)
+     {
+        printf("A is %d\n",A);
+     }
+     
+        A+=2;
+        if(A<40)
+        goto sscet;
+     
+
+      
+    
+}
