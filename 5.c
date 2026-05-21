e5bfaa978571af0a1782b8487454e67dbac0d44d
sabareesw033-droid <sabareesw033@gmail.com>
2026-05-21 21:08:09 +0530

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/*incrementing &decrementing methods*/
+
+int main() {
+    
+        int a,b,c;
+        a=10;
+        printf("the value of before increment is %i  \n" ,a );
+
+        /* increment A*/
+        a=a+1;
+        printf("the value is after increment is  %d \n",a);
+        
+        a=a-1;
+        printf("the value is after decrement is  %d \n",a);
+
+
+
+
+
+}
