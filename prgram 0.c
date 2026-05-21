ac02b83820491cd5d68cfa3d0a800fe867d54281
sabareesw033-droid <sabareesw033@gmail.com>
2026-05-21 21:31:34 +0530

    Add files via upload

---
+++ prgram 0.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a=11;
+	float b=2;
+	float c=a/b;
+	float d=c*2;
+	
+	printf("value of c is %2d: \n",a);
+    printf("value of d is %.2f: \n",c);
+    printf("value of a is %2d :",d);
+
+}
