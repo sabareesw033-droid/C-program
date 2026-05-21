62d56bace22f6fb22be00bc844d53fde77c346e1
sabareesw033-droid <sabareesw033@gmail.com>
2026-05-21 21:09:07 +0530

    Add files via upload

---
+++ 9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    
+    /* simple condition if-else */
+    int A=100;
+    int B=100;
+
+    if (A>B)
+    printf("A = %d and B=%d hence A is greater B",A,B);
+
+    else if (A<B)
+    printf("A =%d and B= %d hence A is smaller B",A,B);
+
+    else
+    printf("A =%d and B= %d hence A & B are equal smaller B",A,B);
+}
